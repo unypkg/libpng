a94a23c972ab0a9e347fcbb3ca3cb5c5a2aec487
michacassola <mich@casso.la>
2024-05-24 21:55:56 +0200

    Update unypkg-libpng-build.sh

--- unypkg-libpng-build.sh
+++ unypkg-libpng-build.sh
@@ -69,7 +69,8 @@ archiving_source
 # shellcheck disable=SC2154
 unyc <<"UNYEOF"
 set -vx
-source /uny/build/functions
+source /uny/git/unypkg/fn
+
 pkgname="libpng"
 
 version_verbose_log_clean_unpack_cd
